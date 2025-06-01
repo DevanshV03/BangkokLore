a6d985cf2ecfe6664c8b75e1a4ed27e241f9097f
DevanshV03 <devansh.verma04@gmail.com>
2025-06-01 12:08:40 +0530

    changes to App.py

--- backend/App.py
+++ backend/App.py
@@ -48,4 +48,5 @@ app.register_blueprint(getstories_bp)
 app.register_blueprint(submitlore_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
